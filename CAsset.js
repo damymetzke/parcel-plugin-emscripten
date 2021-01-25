aa8bdcfbdc76949fc9f293cbaca34ddc99f45bfa
Damy Metzke <damy.metzke@gmail.com>
2021-01-25 12:25:50 +0100

    Change emcc to em++.

--- CAsset.js
+++ CAsset.js
@@ -1,23 +1,22 @@
-const path = require('path');
-const readline = require('readline');
-const url = require('url');
-const commandExists = require('command-exists');
-const { Asset } = require('parcel-bundler');
+const path = require("path");
+const readline = require("readline");
+const url = require("url");
+const commandExists = require("command-exists");
+const { Asset } = require("parcel-bundler");
 
-const childProcess = require('child_process');
+const childProcess = require("child_process");
 const exec = promisify(childProcess.execFile);
-const _fs = require('fs');
+const _fs = require("fs");
 const fs = {
-	copyFile: promisify(_fs.copyFile),
-	readFile: promisify(_fs.readFile)
+  copyFile: promisify(_fs.copyFile),
+  readFile: promisify(_fs.readFile),
 };
-const mkdirp = promisify(require('mkdirp'));
-
+const mkdirp = promisify(require("mkdirp"));
 
 function promisify(fn) {
-  return function(...args) {
-    return new Promise(function(resolve, reject) {
-      fn(...args, function(err, ...res) {
+  return function (...args) {
+    return new Promise(function (resolve, reject) {
+      fn(...args, function (err, ...res) {
         if (err) return reject(err);
 
         if (res.length === 1) return resolve(res[0]);
@@ -26,35 +25,33 @@ function promisify(fn) {
       });
     });
   };
-};
-
-
-function readLine(file){
-	return new Promise((res, rej)=>{
-		try{
-			const lineReader = readline.createInterface({
-				input: _fs.createReadStream(file),
-			});
-
-			let lineCounter = 0;
-			const lines = [];
-			lineReader.on('line', function (line) {
-				if(lineCounter++ >= 1){
-					lineReader.close();
-					return;
-				}
-				res(line);
-				// lines.push(line);
-			});
-			// lineReader.on('close', function() {
-				// res(Array.from(lines))
-			// });
-		} catch(err){
-			rej(err);
-		}
-	})
 }
 
+function readLine(file) {
+  return new Promise((res, rej) => {
+    try {
+      const lineReader = readline.createInterface({
+        input: _fs.createReadStream(file),
+      });
+
+      let lineCounter = 0;
+      const lines = [];
+      lineReader.on("line", function (line) {
+        if (lineCounter++ >= 1) {
+          lineReader.close();
+          return;
+        }
+        res(line);
+        // lines.push(line);
+      });
+      // lineReader.on('close', function() {
+      // res(Array.from(lines))
+      // });
+    } catch (err) {
+      rej(err);
+    }
+  });
+}
 
 let installed = false;
 
@@ -62,109 +59,120 @@ const headerRegex = /^\s*#include\s+"(.+)"/;
 const cRegex = /\.c(?:pp)?$/;
 
 class CAsset extends Asset {
-	constructor(name, pkg, options) {
-		super(name, pkg, options);
-		this.type = 'js'
+  constructor(name, pkg, options) {
+    super(name, pkg, options);
+    this.type = "js";
 
-		this.outPath = path.join(this.options.cacheDir, this.generateBundleName());
-	}
+    this.outPath = path.join(this.options.cacheDir, this.generateBundleName());
+  }
 
-	async pretransform(){
-		const firstLine = await readLine(this.name);
-		this.cFiles = [];
+  async pretransform() {
+    const firstLine = await readLine(this.name);
+    this.cFiles = [];
 
-		const p = path.dirname(this.name)
-		if(firstLine.indexOf("//parcel: ") == 0){
-			this.args = firstLine.substr(10).split(" ");
-			
-			for(let i = this.args.length - 1; i >= 0; i--){
-				if(cRegex.test(this.args[i])){
-					this.cFiles.push(this.args[i]);
-				}
-			}
-		}
-	}
+    const p = path.dirname(this.name);
+    if (firstLine.indexOf("//parcel: ") == 0) {
+      this.args = firstLine.substr(10).split(" ");
 
-	process() {
-		if (this.options.isWarmUp) {
-			return;
-		}
+      for (let i = this.args.length - 1; i >= 0; i--) {
+        if (cRegex.test(this.args[i])) {
+          this.cFiles.push(this.args[i]);
+        }
+      }
+    }
+  }
 
-		return super.process();
-	}
+  process() {
+    if (this.options.isWarmUp) {
+      return;
+    }
 
-	collectDependencies() {
-		const folder = path.dirname(this.name);
-		const getAbs = (v) => path.join(folder, v);
+    return super.process();
+  }
 
-		this.contents.split("\n")
-					 .map((l)=>l.match(headerRegex))
-					 .filter((v)=> v && v[1])
-					 .map((v)=> this.addDependency("./"+v[1], {includedInParent: true, name: getAbs(v[1])}));
+  collectDependencies() {
+    const folder = path.dirname(this.name);
+    const getAbs = (v) => path.join(folder, v);
 
-		for(let i = this.cFiles.length - 1; i >= 0; i--){
-			this.addDependency("./"+this.cFiles[i], {includedInParent: true, name: getAbs(this.cFiles[i])});
-		}
+    this.contents
+      .split("\n")
+      .map((l) => l.match(headerRegex))
+      .filter((v) => v && v[1])
+      .map((v) =>
+        this.addDependency("./" + v[1], {
+          includedInParent: true,
+          name: getAbs(v[1]),
+        })
+      );
 
-	}
+    for (let i = this.cFiles.length - 1; i >= 0; i--) {
+      this.addDependency("./" + this.cFiles[i], {
+        includedInParent: true,
+        name: getAbs(this.cFiles[i]),
+      });
+    }
+  }
 
-	async parse() {
-		const name = this.generateBundleName();
+  async parse() {
+    const name = this.generateBundleName();
 
-		await this.checkEmscripten();
+    await this.checkEmscripten();
 
-		await mkdirp(this.options.cacheDir);
+    await mkdirp(this.options.cacheDir);
 
-		let args = [
-			// '-Os',
-			'-s', "WASM=1",
-			'-s', "MODULARIZE=1",
-			// '-s', 'ASSERTIONS=1',
-			'-s', 'EXTRA_EXPORTED_RUNTIME_METHODS=["ccall", "cwrap"]',
-			this.name,
-			'-o',
-			this.outPath
-		];
+    let args = [
+      // '-Os',
+      "-s",
+      "WASM=1",
+      "-s",
+      "MODULARIZE=1",
+      // '-s', 'ASSERTIONS=1',
+      "-s",
+      'EXTRA_EXPORTED_RUNTIME_METHODS=["ccall", "cwrap"]',
+      this.name,
+      "-o",
+      this.outPath,
+    ];
 
-		if(this.options.minify){
-			args.push("-Os");
-		}
+    if (this.options.minify) {
+      args.push("-Os");
+    }
 
-		if(this.args){
-			args = args.concat(this.args)
-		}
+    if (this.args) {
+      args = args.concat(this.args);
+    }
 
-		await exec('emcc', args, {cwd: path.dirname(this.name)});
+    await exec("em++", args, { cwd: path.dirname(this.name) });
 
-		if(args.includes("WASM=1")){
-			await fs.copyFile(
-				path.join(this.options.cacheDir, name).slice(0,-3)+".wasm",
-				path.join(this.options.outDir, name).slice(0,-3)+".wasm"
-			)
-		}
-	}
+    if (args.includes("WASM=1")) {
+      await fs.copyFile(
+        path.join(this.options.cacheDir, name).slice(0, -3) + ".wasm",
+        path.join(this.options.outDir, name).slice(0, -3) + ".wasm"
+      );
+    }
+  }
 
-	async checkEmscripten() {
-		if(installed) return;
-		try {
-			await commandExists('emcc');
-		} catch (e) {
-			throw new Error(
-				"Emscripten isn't installed!"
-			);
-		}
-		installed = true;
-	}
+  async checkEmscripten() {
+    if (installed) return;
+    try {
+      await commandExists("emcc");
+    } catch (e) {
+      throw new Error("Emscripten isn't installed!");
+    }
+    installed = true;
+  }
 
-	async generate() {
-		const name = this.generateBundleName().slice(0,-3);
-		const publicURL = this.options.publicURL.replace(/\/+$/, "");
+  async generate() {
+    const name = this.generateBundleName().slice(0, -3);
+    const publicURL = this.options.publicURL.replace(/\/+$/, "");
 
-		return {
-			js: (await fs.readFile(this.outPath, "utf8"))
-					.replace(new RegExp(name, "g"), publicURL+"/"+name)
-					// fix https://github.com/kripken/emscripten/issues/5820 !
-					/*
+    return {
+      js: (await fs.readFile(this.outPath, "utf8")).replace(
+        new RegExp(name, "g"),
+        publicURL + "/" + name
+      ),
+      // fix https://github.com/kripken/emscripten/issues/5820 !
+      /*
 
 module\.exports = Module;
 	to
@@ -173,8 +181,8 @@ Module.then = (f) => {delete m['then']; _then(f);};
 module.exports = Module;
 ???
 					*/
-		};
-	}
+    };
+  }
 }
 
 module.exports = CAsset;
